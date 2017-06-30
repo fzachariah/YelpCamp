f1798e47f78d21746e06bb73841611c5f01a07b5
FEBIN ZACHARIAH <febinzachariah6@gmail.com>
2017-06-29 21:08:15 -0400

    AuthenticationFinal

--- app.js
+++ app.js
@@ -12,10 +12,31 @@ var Comment=require("./models/comment");
 var seedDB=require("./seeds");
 app.use(express.static(__dirname + "/public"));
 app.set("view engine","ejs");
+var passport=require("passport");
+var LocalStrategy=require("passport-local");
+
 
 var User=require("./models/user");
 
 seedDB();
+
+
+//
+
+app.use(require("express-session")({secret:"Febin is the best guy",resave:false,saveUninitialized:false}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(function(req,res,next){
+    res.locals.currentUser=req.user;
+    next();
+});
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 
 app.get("/",function(req,res){
     res.render("landing");
@@ -29,7 +50,7 @@ app.get("/campgrounds",function(req,res){
             console.log(err);
         }
         else{
-            res.render("campgrounds/index",{campgrounds:allCampgrounds});
+            res.render("campgrounds/index",{campgrounds:allCampgrounds,currentUser:req.user});
         }
     });
 
@@ -84,7 +105,7 @@ app.get("/campgrounds/:id",function(req, res) {
 });
 
 
-app.get("/campgrounds/:id/comments/new",function(req, res) {
+app.get("/campgrounds/:id/comments/new",isLoggedIn,function(req, res) {
     
     Campground.findById(req.params.id,function(err,campground){
         
@@ -101,7 +122,7 @@ app.get("/campgrounds/:id/comments/new",function(req, res) {
     
 });
 
-app.post("/campgrounds/:id/comments",function(req,res){
+app.post("/campgrounds/:id/comments",isLoggedIn,function(req,res){
     
     
     Campground.findById(req.params.id,function(err,campground){
@@ -122,6 +143,55 @@ app.post("/campgrounds/:id/comments",function(req,res){
     });
     
 });
+
+
+//Auth Routes
+
+app.get("/register",function(req, res) {
+   res.render("register"); 
+});
+
+app.post("/register",function(req, res) {
+    
+    User.register(new User({username:req.body.username}),req.body.password,function(err,user){
+        if(err)
+        {
+            return res.render("register");
+        }
+        passport.authenticate("local")(req,res,function(){
+            
+            res.redirect("/campgrounds");
+            
+        });
+    });
+    
+});
+
+
+app.get("/login",function(req, res) {
+   res.render("login"); 
+});
+
+app.post("/login",passport.authenticate("local",{successRedirect:"/campgrounds",failureRedirect:"/login"}),function(req, res) {
+});
+
+app.get("/logout",function(req, res) {
+    
+    req.logout();
+    res.redirect("/campgrounds");
+    
+    
+});
+
+function isLoggedIn(req,res,next){
+    
+    
+    if(req.isAuthenticated())
+    {
+        return next();
+    }
+    res.redirect("/login");
+}
 
 
 
